3c316b343e4ea5bb9404e91ad4e75db48c63e598
joaogabrocha <80985064+JoaoGabrielBr246@users.noreply.github.com>
2024-01-25 04:15:37 -0300

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,11 @@
 function crip() {
-  let text = document.querySelector(".program__textArea").value;
+  let textArea = document.querySelector(".program__textArea");
+  let text = textArea.value;
+
+  let errorMessage = document.querySelector(".error-message");
 
   if (!/^[a-z\s.,!?]+$/i.test(text) || /[A-ZÀ-ÚÂ-ÛÄ-Ü]/.test(text)) {
-    alert("Por favor, digite apenas letras minúsculas, espaços e pontuações (.,!?)");
+    errorMessage.textContent = "Por favor, digite apenas letras minúsculas, espaços e pontuações (.,!?)";
     return;
   }
 
@@ -17,13 +20,19 @@ function crip() {
   });
 
   exibirResultado();
+
+  // Limpar mensagem de erro se não houver erro
+  errorMessage.textContent = "";
 }
 
 function descrip() {
-  let text = document.querySelector(".program__textArea").value;
+  let textArea = document.querySelector(".program__textArea");
+  let text = textArea.value;
+
+  let errorMessage = document.querySelector(".error-message");
 
   if (!/^[a-z\s.,!?]+$/i.test(text) || /[A-ZÀ-ÚÂ-ÛÄ-Ü]/.test(text)) {
-    alert("Por favor, digite apenas letras minúsculas, espaços e pontuações (.,!?)");
+    errorMessage.textContent = "Por favor, digite apenas letras minúsculas, espaços e pontuações (.,!?)";
     return;
   }
 
@@ -38,43 +47,44 @@ function descrip() {
   });
 
   exibirResultado();
+
+  // Limpar mensagem de erro se não houver erro
+  errorMessage.textContent = "";
 }
 
-  
-  function exibirResultado() {
-    let image = document.querySelector(".program__img");
-    let h1 = document.querySelector(".program__rec__h1");
-    let p = document.querySelector(".program__rec__p");
-    let btn = document.querySelector(".program__rec__btn");
-    let hiddenText = document.querySelector(".program__rec__pHidden");
-  
-    if (listaFrase.length === 0) {
-      image.src = "assets/nomessage.svg";
-      h1.textContent = "Nenhuma mensagem encontrada";
-      hiddenText.textContent = ""
-      p.textContent = "Digite um texto que você deseja criptografar ou descriptografar";
-      btn.setAttribute('hidden', true);
-    } else {
-      image.src = ""; 
-      hiddenText.textContent = listaFrase.join(""); 
-      p.textContent = ""
-      h1.textContent = "";
-      btn.removeAttribute('hidden');
-      hiddenText.removeAttribute('hidden');
-    }
+function exibirResultado() {
+  let image = document.querySelector(".program__img");
+  let h1 = document.querySelector(".program__rec__h1");
+  let p = document.querySelector(".program__rec__p");
+  let btn = document.querySelector(".program__rec__btn");
+  let hiddenText = document.querySelector(".program__rec__pHidden");
+
+  if (listaFrase.length === 0) {
+    image.src = "assets/nomessage.svg";
+    h1.textContent = "Nenhuma mensagem encontrada";
+    hiddenText.textContent = "";
+    p.textContent = "Digite um texto que você deseja criptografar ou descriptografar";
+    btn.setAttribute('hidden', true);
+  } else {
+    image.src = "";
+    hiddenText.textContent = listaFrase.join("");
+    p.textContent = "";
+    h1.textContent = "";
+    btn.removeAttribute('hidden');
+    hiddenText.removeAttribute('hidden');
   }
-  
-  function copyText() {
-    let textToCopy = document.querySelector(".program__rec__pHidden").textContent;
-  
-    if (textToCopy) {
-      navigator.clipboard.writeText(textToCopy)
-        .then(() => {
-          alert("Texto copiado para a área de transferência!");
-        })
-        .catch((err) => {
-          console.error("Erro ao copiar texto: ", err);
-        });
-    }
+}
+
+function copyText() {
+  let textToCopy = document.querySelector(".program__rec__pHidden").textContent;
+
+  if (textToCopy) {
+    navigator.clipboard.writeText(textToCopy)
+      .then(() => {
+        alert("Texto copiado para a área de transferência!");
+      })
+      .catch((err) => {
+        console.error("Erro ao copiar texto: ", err);
+      });
   }
-  
+}
